05434acfab1f004fb12a773b91c650b4b650860b
mercy-rotich <cherotichm182@gmail.com>
2024-09-29 12:25:34 -0700

    Worked on laying the project structure, built the Navbar and worked on routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import "./App.css"
+
+import Home from './pages/Home/Home'
+
+import { BrowserRouter,Route,Routes  } from "react-router-dom"
+import About from './pages/About/About'
+import ContactUs from './pages/ContactUs/ContactUs'
+import Alumini from './pages/Alumini/Alumini'
+import Services from './pages/Services/Services'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={ <Home/> } />
+
+          <Route path='/about-us' element={ <About/> } />
+
+          <Route path='/contact-us' element={ <ContactUs/> } />
+
+          <Route path='/alumini' element={ <Alumini/>
+           } />
+
+          <Route path='/services' element={ <Services/> } />
+
+        </Routes>
+      
+      </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+
+        <div className="container">
+
+            <div className="logo">
+
+                <a href="">LOGO<span>SO</span></a>
+                
+             </div>
+
+             <div className="navigation-wrapper">
+
+                <ul className="nav-ul">
+
+                    <li><Link to="/">HOME</Link></li>
+                    <li><Link to="/about-us">ABOUT US</Link></li>
+                    <li><Link to="/services">SERVICES</Link></li>
+                    <li><Link to="/alumini">ALUMINI</Link></li>
+                    <li><Link to="/contact-us">CONTACT US</Link></li>
+
+                </ul>
+
+             </div>
+
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
